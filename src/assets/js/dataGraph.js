43b3b1b9948ee2b3f61468f7e79ba87fdcc3bb58
Mykhado <mikawestindies@gmail.com>
2022-09-23 23:27:20 +0200

    update tri + add operation

--- src/assets/js/dataGraph.js
+++ src/assets/js/dataGraph.js
@@ -1,13 +1,23 @@
-// <block:setup:1>
+// initialisation de ma courbe
 let montants = [1200, 750, 775, 760, 2560];
 let ajoutMontant = document.getElementById("montant");
 let provenance = document.getElementById("titre");
 let motif = document.getElementById("desc");
 let debitCredit = document.getElementById("operator");
 let envoi = document.querySelector(".btSubmit");
+let grid = document.getElementById("true-container");
 let montantsCount = montants.length + 2;
+let btnallOpe = document.getElementById("allOperation");
+let btncreditOpe = document.getElementById("creditOpe");
+let btndebitOpe = document.getElementById("debitOpe");
+let opevalue = document.querySelectorAll(`.operation`);
+let debitOpeValue = document.getElementsByClassName("operation credit");
+console.log(debitOpeValue);
+let creditOpeValue = document.getElementsByClassName("operation debit");
+console.log(creditOpeValue);
 let valeurMontant;
-
+let valeurProvenance;
+let valeurMotif;
 
 console.log();
 let labels = [];
@@ -21,24 +31,31 @@ const data = {
     {
       // configuration de la courbe
       label: provenance,
+      // nom de chaque point
       data: montants,
+      // tableau des montants
       borderColor: "purple",
+      // couleur de la courbe
       fill: true,
+      // couleur dessous de la courbe
       cubicInterpolationMode: "monotone",
+      // gere la forme de la courbe
     },
   ],
 };
 console.log(data);
-// </block:setup>
+// fin de l'initialisation
 
 // <block:config:0>
 const config = {
   type: "line",
+  // type de la courbe
   data: data,
   options: {
     elements: {
       point: {
         radius: 0,
+        // gere les cercle des points
       },
     },
     responsive: true,
@@ -64,9 +81,9 @@ const config = {
 };
 
 /*Le contexte du canevas HTML */
-context = document.getElementById("myChart").getContext("2d");
+let context = document.getElementById("myChart").getContext("2d");
 /* Création du graphique */
-chart = new Chart(context, config);
+let chart = new Chart(context, config);
 
 /* Générer des données aléatoires */
 function generateData() {
@@ -77,36 +94,80 @@ function generateData() {
   console.log(newtransaction);
 }
 
-let testdata =generateData();
+let testdata = generateData();
 
 function addmoney() {
- 
   valeurMontant = ajoutMontant.value;
   /* Ajoute la valeur en X */
   //   augmenter la capaciter max du tableau correspondant
-  config.data.labels.push(labels.length+1);
-  console.log(config.data.labels)
+  config.data.labels.push(labels.length + 1);
+  console.log(config.data.labels);
 
-  /* Ajoute la valeur */
+  /* Ajoute la valeur de l'operation */
   config.data.datasets[0].data.push(valeurMontant);
 
   /* Rafraichir le graphique */
   chart.update();
-  };
+}
 
-
-
-envoi.addEventListener("click",(e) =>{
-  e.preventDefault() 
- valeurMontant = ajoutMontant.value;
-//   augmenter la capaciter max du tableau correspondant
-  montantsCount ++;
+envoi.addEventListener("click", (e) => {
+  e.preventDefault();
+  valeurMontant = ajoutMontant.value;
+  valeurProvenance = provenance.value;
+  valeurMotif = motif.value;
+  //   augmenter la capaciter max du tableau correspondant
+  montantsCount++;
 
   console.log(valeurMontant);
+
+  // let operator = document.getElementById("operator").value;
+  console.log(valeurProvenance);
+  console.log(valeurMotif);
+
+  grid.innerHTML += `<div class="operation credit">
+       <div class="grid-x grid-padding-x align-middle">
+         <div class="cell shrink">
+           <div class="picto">
+             <img src="./assets/images/sac-dargent.png" alt="credit" />
+           </div>
+         </div>
+         <div class="cell auto">
+           <div>
+           <h2>${valeurProvenance}</h2>
+           <small>${valeurMotif}</small>
+           </div>
+         </div>
+         <div class="cell small-3 text-right">
+           <div>
+           <p class="count">${valeurMontant}€</p>
+             <small>100%</small>
+           </div>
+         </div>
+       </div>
+     `;
+
   addmoney();
- 
   console.log(config.data.labels);
-  console.log (config.data.datasets[0].data);
+  console.log(config.data.datasets[0].data);
+});
+//  test  ajout historique
+function removeClasslist() {
+  btnallOpe.classList.remove("active");
+  btncreditOpe.classList.remove("active");
+  btndebitOpe.classList.remove("active");
+}
 
+btncreditOpe.addEventListener("click", () => {
+  removeClasslist();
+  btncreditOpe.classList.add("active");
+});
 
+btndebitOpe.addEventListener("click", () => {
+  removeClasslist();
+  btndebitOpe.classList.add("active");
+});
+
+btnallOpe.addEventListener("click", () => {
+  removeClasslist();
+  btnallOpe.classList.add("active");
 });
